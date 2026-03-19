9d3e3f53b1cb77671a88b3212c17426b6d32bd69
Shivang Sharma <sharmashivang1999@gmail.com>
2026-03-19 23:57:42 +0530

    feat: Strategy design pattern added

---
+++ Strategy/With/Main.ts
@@ -0,0 +1,46 @@
+interface PaymentStrategy {
+  pay(amount: number): void;
+}
+
+class UpiPayment implements PaymentStrategy {
+  pay(amount: number) {
+    console.log(`${amount} is paid via UPI`);
+  }
+}
+
+class CardPayment implements PaymentStrategy {
+  pay(amount: number) {
+    console.log(`${amount} is paid via Card`);
+  }
+}
+
+class WalletPayment implements PaymentStrategy {
+  pay(amount: number) {
+    console.log(`${amount} is paid via Wallet`);
+  }
+}
+
+class PaymentService {
+  private paymentStrategy: PaymentStrategy;
+
+  constructor(paymentStrategy: PaymentStrategy) {
+    this.paymentStrategy = paymentStrategy;
+  }
+
+  setStrategy(paymentStrategy: PaymentStrategy) {
+    this.paymentStrategy = paymentStrategy;
+  }
+
+  pay(amount: number): void {
+    this.paymentStrategy.pay(amount);
+  }
+}
+
+const paymentService = new PaymentService(new UpiPayment());
+paymentService.pay(200);
+
+paymentService.setStrategy(new CardPayment());
+paymentService.pay(500);
+
+paymentService.setStrategy(new WalletPayment());
+paymentService.pay(1000);
---
+++ Strategy/With/UML.md
@@ -0,0 +1,3 @@
+## UML of Strategy Design Pattern
+
+![alt text](image.png)
